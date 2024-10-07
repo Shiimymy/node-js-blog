c3376849786c8bbac323fa71f3cce784b3544c6a
Eugénie <eugenie.b24@hotmail.fr>
2024-10-07 21:05:59 +0100

    Add home page path

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const path = require("path");
 
 const app = new express();
+
+app.use(express.static("public")); // get the static in public dir
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/index.html")); // create html home page
+});
 
 app.listen(4000, () => {
   console.log("App listening");
